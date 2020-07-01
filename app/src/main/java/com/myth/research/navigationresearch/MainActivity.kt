addd57437a88aa721bdfb8af84ac0085274122ea
yanweiqiang04496 <yanweiqiang04496@hellobike.com>
2020-07-01 19:51:41 +0800

    Add project

---
+++ app/src/main/java/com/myth/research/navigationresearch/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.myth.research.navigationresearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.navHost)
+        bottom_navigation.setupWithNavController(navController)
+    }
+}
